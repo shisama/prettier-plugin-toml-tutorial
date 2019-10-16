a14e70f1d80ac10e76bdcc2583fb48a8dda109fb
shisama <shisama07@gmail.com>
2019-10-16 18:34:00 +0900

    Add my implementation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const parser = require('toml/lib/parser')
 const {
   doc: {
-    builders: { concat }
+    builders: { concat, hardline, group, indent, join, softline, line }
   }
 } = require('prettier')
 
@@ -28,6 +28,32 @@ function printToml(path, options, print) {
   }
 
   switch (node.type) {
+    case 'Assign':
+      return concat([node.key, ' = ', path.call(print, 'value'), hardline])
+    case 'String':
+      // return concat(['"', node.value, '"'])
+      return `"${node.value}"`
+    case 'Integer':
+    case 'Boolean':
+      return node.value.toString()
+    case 'Date':
+      return node.value.toISOString()
+    case 'ObjectPath':
+      return concat(['[', node.value.join('.'), ']', hardline])
+    case 'Array':
+      return group(
+        concat([
+          '[',
+          indent(
+            concat([
+              softline,
+              join(concat([',', line]), path.map(print, 'value'))
+            ])
+          ),
+          softline,
+          ']'
+        ])
+      )
     default:
       return ''
   }
